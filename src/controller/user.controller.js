c618244a1dbcd31fac7bd943e7b042fb96e0e1c4
Stanislav Minko <minkostas@gmail.com>
2023-06-08 12:45:04 +0300

    taskManager: userController-buildResponse

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const route = express.Router();
 const { getAllUser, createUser, updateUser, deleteUser, patchUser } = require('../service/user.service');
+const buildResponse = require('../helper/buildResponse');
 
 route.get('/', async (req, res) => {
   try {
     const data = await getAllUser();
-    res.send(data);
+    buildResponse(res, 200, data);
   } catch (error) {
-    res.send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 route.post('/', async (req, res) => {
   try {
     const { name, surname, email, pwd } = req.body;
     const data = await createUser(name, surname, email, pwd);
-    res.send(data);
+    buildResponse(res, 200, data);
   } catch (error) {
-    res.send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 route.put('/:id', async (req, res) => {
@@ -24,9 +25,9 @@ route.put('/:id', async (req, res) => {
     const { id } = req.params;
     const { name, surname, email, pwd } = req.body;
     const data = await updateUser(id, name, surname, email, pwd);
-    res.send(data);
+    buildResponse(res, 200, data);
   } catch (error) {
-    res.send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 route.delete('/:id', async (req, res) => {
@@ -34,9 +35,9 @@ route.delete('/:id', async (req, res) => {
     const { id } = req.params;
     const { name, surname, email, pwd } = req.body;
     const data = await deleteUser(id, name, surname, email, pwd);
-    res.send(data);
+    buildResponse(res, 200, data);
   } catch (error) {
-    res.send(error.message);
+    buildResponse(res, 404, error.message);
   }
 });
 
@@ -45,9 +46,9 @@ route.patch('/:id', async(req,res)=>{
     const{id} = req.params;
     const clientData = req.body;
     const data = await patchUser(id, clientData);
-    res.send(data);
+    buildResponse(res, 200, data);
   } catch (error) {
-
+    buildResponse(res, 404, error.message);
   }
 })
 
