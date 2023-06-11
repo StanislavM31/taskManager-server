ccdf8784ca6b1a10e48ed9b89d84d47a2ac63b58
Stanislav Minko <minkostas@gmail.com>
2023-06-11 13:21:19 +0300

    taskManager: autorizationUser (compare pwd - hash+pwd)

--- src/controller/api.controller.js
+++ src/controller/api.controller.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const { buildResponse } = require("../helper/buildResponse");
 const route = express.Router();
-const {createUser} = require('../service/api.service');
+const {createUser, authorizationUser} = require('../service/api.service');
 
-route.post('/', async (req,res)=>{
+route.post('/registration', async (req,res)=>{
     try {
         const{name, surname, email, pwd} = req.body;
         const data = await createUser(name, surname, email, pwd);
         res.send(data);
     } catch (error) {
         res.send(error.message);
+    }
+})
+
+route.post('/auth', async(req,res)=>{
+    try {
+        const {email, pwd} = req.body;
+        const data = await authorizationUser(email, pwd);
+        buildResponse(res, 200, data);
+    } catch (error) {
+        buildResponse(res, 404,error.message)
     }
 })
 
--- src/service/api.service.js
+++ src/service/api.service.js
@@ -1,16 +1,24 @@
-const {createUserDB, getUserByEmailDB} = require("../repository/api.repository");
+const { createUserDB, getUserByEmailDB } = require('../repository/api.repository');
 
 const bcrypt = require('bcrypt');
 
 const salt = 2;
 
-async function createUser(name, surname, email, pwd){
-    const  foundUser = await getUserByEmailDB(email);
-    if(foundUser.length) throw new Error('user-email is already exist ');
+async function createUser(name, surname, email, pwd) {
+  const foundUser = await getUserByEmailDB(email);
+  if (foundUser.length) throw new Error('user-email is already exist ');
 
-    const hashedPassword = await bcrypt.hash(pwd, salt);
-    const data = await createUserDB(name, surname, email, hashedPassword);
-    return data
+  const hashedPassword = await bcrypt.hash(pwd, salt);
+  const data = await createUserDB(name, surname, email, hashedPassword);
+  return data;
 }
+async function authorizationUser(email, pwd) {
+  const foundUser = await getUserByEmailDB(email);
+  if (!foundUser.length) throw new Error("user not found. Authorization can't be done");
 
-module.exports = {createUser}
+  const bool = await bcrypt.compare(pwd, foundUser[0].pwd);
+  if (!bool) throw new Error('вы ввели неправильный пароль. Авторизация невозможна');
+  //пароли не совпадают hash из db и введенный пароль
+  return foundUser;
+}
+module.exports = { createUser, authorizationUser };
